662bbf574911062444e55c509c69ed28e31b8529
neko-sennin <135305310+neko-sennin@users.noreply.github.com>
2023-06-26 09:08:27 +0000

    routing

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,17 +3,18 @@ class ListsController < ApplicationController
     @list = List.new
   end
   
-   def create
+  def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
-    
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
